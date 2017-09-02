92282d3977117689b5075b96a04a2bb54edc6aed
pariyar <sunilpariyar@hotmail.com>
2017-09-02 13:29:47 +1000

    Initial working version

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+            <h1>This is a app components</h1>
+            <p>This is a paragraph</p>
+            <p>This is another paragraph</p>
+            <h2>hello world!!!</h2>
+            </div>
+        );
+    }
+}
